3b9ad7537725dbfb30eb13f0b37ec9d98177bb6e
Akagi201 <akagi201@gmail.com>
2016-07-22 18:34:41 +0800

    fix typo

--- doc.go
+++ doc.go
@@ -1,3 +1,3 @@
-// Package utilgo is a wrapper around various self-contained subpackages, around
+// Package utilgo is a wrapper around various self-contained subpackages, and
 // doesn't actually contain anything in it.
 package utilgo
